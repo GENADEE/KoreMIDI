65e7fae398566119dacc68715c849f9b5907f5a3
Adam Nemecek <adamnemecek@gmail.com>
2017-04-07 16:56:21 -0700

    * stuff

---
+++ Sources/MIDIProject.swift
@@ -0,0 +1,15 @@
+//
+//  MIDIProject.swift
+//  KoreMIDI
+//
+//  Created by Adam Nemecek on 4/7/17.
+//
+//
+
+import Foundation
+
+class MIDIProject {
+    let sequence: MIDISequence
+    
+    
+}
