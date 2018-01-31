0ec62d232d2b1eea54680ab402239389921feca0
Adam Nemecek <adamnemecek@gmail.com>
2018-01-31 14:21:25 -0800

    * refactoring

--- Sources/MIDIMetaTimeSignatureEvent.swift
+++ Sources/MIDIMetaTimeSignatureEvent.swift
@@ -30,13 +30,30 @@ extension AudioToolbox.MIDIMetaEvent {
     }
 }
 
-extension AudioToolbox.MIDIMetaEvent {
-    static var headerSize: Int {
+///
+/// this is for retrieving, not for
+///
+
+internal protocol StaticSizeMessage {
+    static var staticSize: Int { get }
+}
+internal protocol DynamicSizeMessage : StaticSizeMessage {
+    var dynamicSize: Int { get }
+}
+
+extension DynamicSizeMessage {
+    var totalSize : Int {
+        return Self.staticSize + dynamicSize
+    }
+}
+
+extension AudioToolbox.MIDIMetaEvent: DynamicSizeMessage {
+    static var staticSize: Int {
         return MemoryLayout<AudioToolbox.MIDIMetaEvent>.size - MemoryLayout<UInt8>.size
     }
 
     var dynamicSize: Int {
-        return AudioToolbox.MIDIMetaEvent.headerSize + Int(dataLength)
+        return Int(dataLength)
     }
 }
 
@@ -44,7 +61,15 @@ internal protocol MIDIMetaEventType {
     var size: Int { get }
 }
 
-internal struct MIDIMetaEvent2 {
+extension Data {
+    init<M: DynamicSizeMessage>(message: UnsafePointer<M>) {
+        self.init(bytes: message, count: message.pointee.dynamicSize)
+    }
+}
+
+internal struct MIDIMetaEvent2<M: DynamicSizeMessage> {
+
+    /// I hate this structure a lot, it's impossible to do cleanly.
     enum Subtype: UInt8 {
         case sequenceNumber = 0x00,
         textEvent = 0x01,
@@ -64,14 +89,26 @@ internal struct MIDIMetaEvent2 {
         invalid                    = 0x66
     }
 
+//    var metaEventType: Subtype {
+//        get {
+//            return Subtype(rawValue: ptr.pointee.metaEventType)!
+//        }
+//        set {
+//            ptr.pointee.metaEventType = newValue.rawValue
+//        }
+//    }
+
     private var data: Data
+//    private let ptr: UnsafeMutablePointer<>
 
+    init(type: Subtype, message: inout M) {
+        self.data = Data(message: &message)
+//        let header = AudioToolbox.MIDIMetaEvent.headerSize
+//        let alloc = header + data.count
+//        self.data = Data(
 
-
-    init(type: Subtype, data: Data) {
-        let header = AudioToolbox.MIDIMetaEvent.headerSize
-        let alloc = header + data.count
         fatalError()
+//        ptr = self.data.withUnsafeMutableBytes { $0 }
     }
 
     func insert(to: MIDIMetaTrack) {
